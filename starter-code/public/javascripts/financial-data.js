7b71a98f6fdef79eccac791401a573f06f0b0a6d
seabysalt <seabysalt@gmail.com>
2019-07-04 14:40:15 +0200

    done w/o all bonus

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,40 @@
+const getBtcValues = () => {
+
+  const fromDate = document.getElementById("date1").value;
+  const toDate = document.getElementById("date2").value;
+  
+  const url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`;
+
+  axios
+    .get(url)
+    .then(response => {
+      const data = response.data;
+      const dates = Object.keys(data.bpi);
+      const bitcoinValue = Object.values(data.bpi);
+
+      console.log(dates, bitcoinValue);
+
+      const ctx = document.getElementById("bitcoinChart").getContext("2d");
+
+      const myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: dates,
+          datasets: [
+            {
+              label: "Bitcoin price",
+              data: bitcoinValue,
+              backgroundColor: "rgba(0, 183, 255, 0.678)"
+            }
+          ]
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+getBtcValues();
+
+document.getElementById("date2").onchange = () => { getBtcValues() };
